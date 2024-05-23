eea7aa9663d1bad9026d6e8d22a11e1a7f6dba4c
d4nitrix13 <danielperezdev@proton.me>
2024-05-23 16:04:28 -0600

    añadido variable.sh

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Autor: Daniel Benjamin Perez Morales
+# GitHub: https://github.com/DanielPerezMoralesDev13
+# Correo electrónico: danielperezdev@proton.me 
+nombre="D4nitrix13"
+echo $nombre
